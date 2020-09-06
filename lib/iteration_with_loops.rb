c59203c6120d47021d49e698ec7402b29e8f8e27
Rodrigo Rojas <carlorrojas@gmail.com>
2020-09-06 22:23:09 +0000

    Automatically backed up by Learn

---
+++ lib/iteration_with_loops.rb
@@ -0,0 +1,17 @@
+def find_min_in_nested_arrays(src)
+    outer_index = 0
+    low_temp_results = []
+      while outer_index < src.count do
+        inner_index = 0
+        lowest_temperature = 200
+          while inner_index < src[outer_index].count do
+            if src[outer_index][inner_index] < lowest_temperature
+            lowest_temperature = src[outer_index][inner_index]
+            end
+            low_temp_results << lowest_temperature
+            inner_index += 1
+            end
+        outer_index += 1
+      end
+      low_temp_results
+  end
